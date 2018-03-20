9472abb1f385211cdc4ff728334cd25a4432f827
Alexander Freas <akfreas@gmail.com>
2018-03-20 12:32:40 +0100

    Added barebones setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ContentfulLocationUtils',
+      version='0.1',
+      description='Import locations from OSM and other sources to Contentful space meant for storing location data.',
+      url='https://github.com/akfreas/ContentfulLocationUtils',
+      author='Alexander Freas',
+      author_email='alex@sashimiblade.com',
+      license='MIT',
+      packages=['contentful_utils'],
+      zip_safe=False)
